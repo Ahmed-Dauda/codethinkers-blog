9ef85deda602a1f30537f7c0d07e1416384b5324
Ahmed-Dauda <addatainsight@gmil.com>
2022-06-27 20:00:31 +0100

    Added files and changes required for deployment to heroku

--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_post = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
