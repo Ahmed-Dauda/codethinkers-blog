2a658883f6e89b3dd48ae90233b897ee2e74d7c4
Ahmed-Dauda <addatainsight@gmil.com>
2022-06-28 13:00:18 +0100

    Added files and changes required for deployment to heroku

--- users/admin.py
+++ users/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Profile
 
 class ProfileAdmin(admin.ModelAdmin):
-    search_fields = ('user__email', 'user__username')
-    list_display = ('user',)
-    list_filter = ('user__is_staff', 'user__is_superuser')
+    search_fields = ('owner__email', 'owner__username')
+    list_display = ('owner',)
+    list_filter = ('owner__is_staff', 'owner__is_superuser')
 
 
 admin.site.register(Profile, ProfileAdmin)
