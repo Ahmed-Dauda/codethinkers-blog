fd2bf55da7dd26c2a453c390b41fcd7c6db439ed
Ahmed-Dauda <addatainsight@gmil.com>
2022-06-28 15:03:05 +0100

    Update ALLOWED_HOSTS with site and development server URL

--- posts/models.py
+++ posts/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     date_posted = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Profile
+# from .models import Profile
 
-class ProfileAdmin(admin.ModelAdmin):
-    search_fields = ('owner__email', 'owner__username')
-    list_display = ('owner',)
-    list_filter = ('owner__is_staff', 'owner__is_superuser')
+# class ProfileAdmin(admin.ModelAdmin):
+#     search_fields = ('owner__email', 'owner__username')
+#     list_display = ('owner',)
+#     list_filter = ('owner__is_staff', 'owner__is_superuser')
 
 
-admin.site.register(Profile, ProfileAdmin)
+# admin.site.register(Profile, ProfileAdmin)
 
