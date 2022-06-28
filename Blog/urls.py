efcb387165a299f97a5bd0fa3358a93864d6dfb3
Ahmed-Dauda <addatainsight@gmil.com>
2022-06-28 15:11:33 +0100

    Update ALLOWED_HOSTS with site and development server URL

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from users.views import profile
+# from users.views import profile
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -11,7 +11,7 @@ urlpatterns = [
     path('signup/', include('users.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('profile/', profile, name='profile'),
+    # path('profile/', profile, name='profile'),
 ]
 
 if settings.DEBUG:
