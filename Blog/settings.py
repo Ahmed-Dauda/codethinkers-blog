b9f36c20950cc76906f37e7504833961f5804914
Ahmed-Dauda <addatainsight@gmil.com>
2022-06-28 12:45:05 +0100

    make it better

--- Blog/settings.py
+++ Blog/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'crispy_forms',
 ]
 
+# AUTH_USER_MODEL = 'users.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
